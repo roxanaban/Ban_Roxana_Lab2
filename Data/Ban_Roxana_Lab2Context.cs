6993ebc2aa741d86bddce4bd4241a6349d48a5ed
40746 <40746@LAPTOP-QR9I7F0L>
2023-10-22 22:07:56 +0300

    Add project files.

---
+++ Data/Ban_Roxana_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ban_Roxana_Lab2.Models;
+
+namespace Ban_Roxana_Lab2.Data
+{
+    public class Ban_Roxana_Lab2Context : DbContext
+    {
+        public Ban_Roxana_Lab2Context (DbContextOptions<Ban_Roxana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ban_Roxana_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Ban_Roxana_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
